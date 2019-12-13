8a88141db3676e11018d8200f465979d76c90e38
Harjot Kaur <harjot1471@gmail.com>
2019-12-13 12:09:38 +0530

    adding files

---
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -0,0 +1,57 @@
+import FWCore.ParameterSet.Config as cms
+
+# helper fuctions
+from HLTrigger.Configuration.common import *
+
+# add one customisation function per PR
+# - put the PR number into the name of the function
+# - add a short comment
+# for example:
+
+# CCCTF tuning
+# def customiseFor12718(process):
+#     for pset in process._Process__psets.values():
+#         if hasattr(pset,'ComponentType'):
+#             if (pset.ComponentType == 'CkfBaseTrajectoryFilter'):
+#                 if not hasattr(pset,'minGoodStripCharge'):
+#                     pset.minGoodStripCharge = cms.PSet(refToPSet_ = cms.string('HLTSiStripClusterChargeCutNone'))
+#     return process
+
+
+def customiseFor2017DtUnpacking(process):
+    """Adapt the HLT to run the legacy DT unpacking
+    for pre2018 data/MC workflows as the default"""
+
+    if hasattr(process,'hltMuonDTDigis'):
+        process.hltMuonDTDigis = cms.EDProducer( "DTUnpackingModule",
+            useStandardFEDid = cms.bool( True ),
+            maxFEDid = cms.untracked.int32( 779 ),
+            inputLabel = cms.InputTag( "rawDataCollector" ),
+            minFEDid = cms.untracked.int32( 770 ),
+            dataType = cms.string( "DDU" ),
+            readOutParameters = cms.PSet(
+                localDAQ = cms.untracked.bool( False ),
+                debug = cms.untracked.bool( False ),
+                rosParameters = cms.PSet(
+                    localDAQ = cms.untracked.bool( False ),
+                    debug = cms.untracked.bool( False ),
+                    writeSC = cms.untracked.bool( True ),
+                    readDDUIDfromDDU = cms.untracked.bool( True ),
+                    readingDDU = cms.untracked.bool( True ),
+                    performDataIntegrityMonitor = cms.untracked.bool( False )
+                    ),
+                performDataIntegrityMonitor = cms.untracked.bool( False )
+                ),
+            dqmOnly = cms.bool( False )
+        )
+
+    return process
+
+
+# CMSSW version specific customizations
+def customizeHLTforCMSSW(process, menuType="GRun"):
+
+    # add call to action function in proper order: newest last!
+    # process = customiseFor12718(process)
+
+    return process
